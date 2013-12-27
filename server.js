b3e0b5f33b5840e3ffebe016c9ed3e84bc48714e
Gustaf Mäkinen Nordquist <gustafmakinennordquist@gmail.com>
2013-12-27 15:03:30 +0100

    test

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ function of(unit){
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n'+url.pathname);
+  res.end('Hello World\n'+req.url);
 }).listen(1337, '192.168.1.80');
 console.log('Server running at http://127.0.0.1:1337/');
