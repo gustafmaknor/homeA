bafb4d6acccba9d3451fd4c47372aa6b39a09ca7
Gustaf Mäkinen Nordquist <gustafmakinennordquist@gmail.com>
2013-12-27 13:35:06 +0100

    test

--- tdtool.js
+++ tdtool.js
@@ -1,4 +1,4 @@
-var sys = require('sys')
+var sys = require('sys');
 var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
+function puts(error, stdout, stderr) { console.log(stdout) }
 exec("tdtool -n 2", puts);
